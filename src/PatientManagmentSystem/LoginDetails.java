1607d4a9ec10c6db09b353c53fc48c91e0937795
Theon <Theon@10.211.78.193>
2019-12-18 23:16:04 +0000

    Created the project and main class, also created the jframe for the login screen (no data checking added yet).

---
+++ src/PatientManagmentSystem/LoginDetails.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PatientManagmentSystem;
+
+/**
+ *
+ * @author Theon
+ */
+public class LoginDetails {
+    
+    private String UserID;
+    private String UserPassword;
+    
+}
